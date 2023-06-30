b56d05ef208c1919474f4572f4e34312f3afa1fe
yaemsaeng <yaemsaeng2001@gmail.com>
2023-06-30 13:57:30 +0700

    update router and models and DB

--- model/models.py
+++ model/models.py
@@ -7,6 +7,30 @@ class insert_base64(BaseModel):
     class Config:
         schema_extra = {
             "example" : {
-                "base64":"base64_text",
+                "base64":"input_base64_text",
             }
         }
+
+class insert_chat_name(BaseModel):
+    customer_id: str
+    chat_name: str
+    chat_history: dict = {}
+
+    class Config:
+         schema_extra = {
+                "example" : {
+                    "customer_id" : "input_customer_id",
+                    "chat_name":"input_chat_name",
+                }
+            }
+
+class update_chat_name(BaseModel):
+    chat_name: str
+    
+    class Config:
+        schema_extra = {
+            "example" : {
+                "chat_name":"input_new_chat_name",
+            }
+        }
+    
--- route/routes.py
+++ route/routes.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter,UploadFile,File
+from fastapi import APIRouter
+from config.db import collection
 import firebase_admin
 from firebase_admin import credentials, storage
 import base64
-from model.models import insert_base64
+from model.models import insert_base64,insert_chat_name,update_chat_name
 import requests
+from datetime import datetime
 
 Router = APIRouter()
 
@@ -34,11 +36,88 @@ async def create_upload_file(data: insert_base64):
     return {'message': 'Upload successful', 'url': url}
 
 @Router.get("/chatgptresponse", tags=["ChatBot"])
-def get_chat_response(query: str, customer_id: str):
+async def get_chat_response(query: str, customer_id: str):
     url = f"https://mekhav-2e2xbtpg2q-uc.a.run.app/chatgptresponse?query={query}&customer_id={customer_id}"
     response = requests.get(url)
     json_data = response.json()
     return json_data
 
+@Router.get("/all_chat_name", tags=["all_chat_name"])
+async def all_chat_histoy(customer_id: str):
+    # ดึงข้อมูลทั้งหมดจากเอกสารที่มี customer_id เป็น "1234"
+    result = collection.find({"customer_id": customer_id}, {"chat_name": 1})
 
+    # สร้างรายการว่างเพื่อเก็บ chat_history
+    all_chat_name = []
+
+    # วนลูปผลลัพธ์และเพิ่ม chat_history เข้าไปในรายการ
+    for doc in result:
+        chat_history = doc.get("chat_name")
+        all_chat_name.append(chat_history)
+
+    return {"all_chat_name": all_chat_name}
+
+
+@Router.get("/show_chat_history", tags=["show_chat_history"])
+async def all_chat_histoy(customer_id: str,chat_name:str):
+    result = collection.find({"customer_id": customer_id, "chat_name": chat_name})
+
+    show_chat_history = []
+
+    for doc in result:
+        chat_history = doc.get("chat_history")
+        show_chat_history.append(chat_history)
+
+    return show_chat_history
+
+@Router.post("/upload_NewChat", tags=["upload_NewChat"])
+async def upload_data(data: insert_chat_name):
+    collection.insert_one(dict(data))
+    
+
+@Router.post("/insert_Chat_history", tags=["insert_Chat_history"])
+async def insert_Chat_history(chat_name:str,message_user:str,message_bot) :
+
+    # สร้างเวลาปัจจุบัน
+    current_timestamp = datetime.now().isoformat()
+    # ส่วนที่ต้องการเพิ่ม
+    chat_history_update = {
+        "message_user": message_user,
+        "message_bot": message_bot,
+         "timestamp": current_timestamp
+    }
+    document = collection.find_one({"chat_name": chat_name})
+    if document:#ถ้ามีข้อมูล
+        # หาดัชนีใหม่ที่ต้องการสร้าง
+        new_index = str(len(document.get("chat_history", {})))
+
+        # อัปเดตเอกสาร
+        result = collection.update_one(
+            {"chat_name": chat_name},
+            {"$set": {"chat_history." + new_index: chat_history_update}}
+        )
+
+    # ตรวจสอบว่ามีการอัปเดตสำเร็จหรือไม่
+    if result.modified_count == 1:
+        return {"message": "Chat history updated"}
+    else:
+        return {"message": "Chat not found"}
+
+
+@Router.put("/update_chat_name", tags=["data_update"])
+async def update_chat_name(data:update_chat_name,customer_id: str,chat_name:str):
+    collection.find_one_and_update(
+        {
+          "customer_id": customer_id, 
+           "chat_name": chat_name
+        }, 
+        {
+         "$set": dict(data)
+        })
+
+
+@Router.delete("/delete" ,tags=["data_delete"])
+async def delete(customer_id: str,chat_name:str):
+    collection.find_one_and_delete({"customer_id": customer_id, "chat_name": chat_name})
+    
 
