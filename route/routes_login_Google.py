b2364f9d7a33a087da6105216d0094f1f47dee38
yaemsaeng <yaemsaeng2001@gmail.com>
2023-07-03 16:56:06 +0700

    update

--- route/routes_login_Google.py
+++ route/routes_login_Google.py
@@ -1,25 +1,32 @@
 from fastapi import APIRouter, FastAPI, Depends, HTTPException, status, Request
 from fastapi.responses import RedirectResponse
 from google_auth_oauthlib.flow import Flow
+import google.auth.transport.requests
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware import Middleware
 from starlette.requests import Request as StarletteRequest
 from google.oauth2 import id_token
 from google.auth.transport import requests
+import requests
 import os
 from pathlib import Path
 
+from config.db import collection_account
+
 app = FastAPI()
-Router = APIRouter()
+RouterGoogle = APIRouter()
+
+
 app.add_middleware(SessionMiddleware, secret_key="dVu9jfC1PPVGRkq-X5nKaP_vDHC63CxQ2K4W0QVpFJo", session_cookie="user_session")
 
-client_secrets_file = "../client_secret.json"
+client_secrets_file = "client_secret.json"
 
 GOOGLE_CLIENT_ID = "642643535438-mm2947mq2360qr4429tmcjec7lje530j.apps.googleusercontent.com"
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
-    scopes=["openid", "email", "profile"],
-    redirect_uri="http://localhost:4200/callback"
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="https://doc-search-af5d.onrender.com/google/callback"
 )
 
 def login_is_required(func):
@@ -44,9 +51,8 @@ async def callback(request: StarletteRequest):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="State does not match!")
 
     credentials = flow.credentials
-    request_session = requests.RequestsSession()
-    cached_session = requests.session.CachedSession(request_session)
-    token_request = requests.Request(session=cached_session)
+    request_session = requests.Session()
+    token_request = google.auth.transport.requests.Request(session=request_session)
 
     id_info = id_token.verify_oauth2_token(
         id_token=credentials.id_token,
@@ -56,7 +62,20 @@ async def callback(request: StarletteRequest):
 
     request.session["google_id"] = id_info.get("sub")
     request.session["name"] = id_info.get("name")
-    return RedirectResponse("/")
+
+    query_params = {
+        "google_id": id_info.get("sub"),
+        "name": id_info.get("name")
+    }
+
+    redirect_url = "https://doc-search-af5d.onrender.com/?" + "&".join(f"{key}={value}" for key, value in query_params.items())
+
+
+    if(collection_account.find_one({"sub": id_info.get("sub")}) is None):
+        user = collection_account.insert_one(id_info)
+
+    return RedirectResponse(redirect_url)
+
 
 # @app.get("/logout")
 # async def logout(request: StarletteRequest):
@@ -66,3 +85,8 @@ async def callback(request: StarletteRequest):
 # @app.get("/protected_area")
 # async def protected_area(request: StarletteRequest):
 #     return f"Hello {request.session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+
+
+# @app.get("/")
+# async def index():
+#     return "Hello World <a href='/login'><button>Login</button></a>"
